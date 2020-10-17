d2949051d2ac11074a1aabd36421f442aca90565
pol134 <62601755+pol134@users.noreply.github.com>
2020-10-17 17:15:15 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,9 +2,8 @@
 // first : local repo modification 
 // second: github modification
 
-added tell her world
->>>>>>> origin/master
 print ("hello world")
 print ("tell your world")
 print ("tell his world")
 print ("tell her world")
+print ("tell my world")
